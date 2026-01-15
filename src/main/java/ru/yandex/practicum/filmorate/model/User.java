ab21738e156d7280c7a0a64b2608ee3d4675f868
VeraBes <bespalaya.vera@gmail.com>
2026-01-15 20:47:15 +0300

    редактирование стиля

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,8 +3,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PastOrPresent;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDate;
 
