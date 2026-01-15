fe8feee81a19a3ca9df4f43e4f3e533b6d8ce149
VeraBes <bespalaya.vera@gmail.com>
2026-01-15 20:51:57 +0300

    редактирование стиля

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -16,71 +16,71 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @SpringBootTest
 class FilmorateApplicationTests {
 
-	@Test
-	void contextLoads() {
-		User user = new User();
-		user.setId(1);
-		user.setEmail("friend@common.ru");
-		user.setLogin(" ");
-		user.setName("common");
-		user.setBirthday(LocalDate.of(2000, 8, 20));
-		UserController userController = new UserController();
-		assertThrows(ValidationException.class, ()-> userController.createUser(user));
-	}
+    @Test
+    void contextLoads() {
+        User user = new User();
+        user.setId(1);
+        user.setEmail("friend@common.ru");
+        user.setLogin(" ");
+        user.setName("common");
+        user.setBirthday(LocalDate.of(2000, 8, 20));
+        UserController userController = new UserController();
+        assertThrows(ValidationException.class, () -> userController.createUser(user));
+    }
 
-	@Test
-	void testName() {
-		User user = new User();
-		user.setId(1);
-		user.setEmail("friend@common.ru");
-		user.setLogin("common");
-		user.setName("");
-		user.setBirthday(LocalDate.of(2000, 8, 20));
-		UserController userController = new UserController();
-		userController.createUser(user);
-		String actualUserName = user.getName();
-		assertEquals("common", actualUserName);
-	}
+    @Test
+    void testName() {
+        User user = new User();
+        user.setId(1);
+        user.setEmail("friend@common.ru");
+        user.setLogin("common");
+        user.setName("");
+        user.setBirthday(LocalDate.of(2000, 8, 20));
+        UserController userController = new UserController();
+        userController.createUser(user);
+        String actualUserName = user.getName();
+        assertEquals("common", actualUserName);
+    }
 
-	@Test
-	void testEmail() {
-		User user = new User();
-		user.setId(1);
-		user.setLogin("common");
-		user.setName("common");
-		user.setBirthday(LocalDate.of(2000, 8, 20));
-		user.setEmail("friendcommon.ru");
-		UserController userController = new UserController();
-		ValidationException exception = assertThrows(
-				ValidationException.class,
-				() -> userController.createUser(user)
-		);
-		assertEquals("Адрес электронной почты должен содержать символ @",
-				exception.getMessage());
-	}
+    @Test
+    void testEmail() {
+        User user = new User();
+        user.setId(1);
+        user.setLogin("common");
+        user.setName("common");
+        user.setBirthday(LocalDate.of(2000, 8, 20));
+        user.setEmail("friendcommon.ru");
+        UserController userController = new UserController();
+        ValidationException exception = assertThrows(
+                ValidationException.class,
+                () -> userController.createUser(user)
+        );
+        assertEquals("Адрес электронной почты должен содержать символ @",
+                exception.getMessage());
+    }
 
-	@Test
-	void testFilmNameNotNull() {
-		Film film = new Film();
-		film.setId(1);
-		film.setName(" ");
-		film.setDuration(100);
-		film.setDescription("Film description");
-		film.setReleaseDate(LocalDate.of(2000, 8, 20));
-		FilmController filmController = new FilmController();
-		assertThrows(ValidationException.class, ()-> filmController.createFilm(film));
-	}
+    @Test
+    void testFilmNameNotNull() {
+        Film film = new Film();
+        film.setId(1);
+        film.setName(" ");
+        film.setDuration(100);
+        film.setDescription("Film description");
+        film.setReleaseDate(LocalDate.of(2000, 8, 20));
+        FilmController filmController = new FilmController();
+        assertThrows(ValidationException.class, () -> filmController.createFilm(film));
+    }
 
-	@Test
-	void testFilmDuration() {
-		Film film = new Film();
-		film.setId(1);
-		film.setName("Film name");
-		film.setDuration(-50);
-		film.setDescription("Film description");
-		film.setReleaseDate(LocalDate.of(2000, 8, 20));
-		FilmController filmController = new FilmController();
-		assertThrows(ValidationException.class, ()-> filmController.createFilm(film));
-	}
+    @Test
+    void testFilmDuration() {
+        Film film = new Film();
+        film.setId(1);
+        film.setName("Film name");
+        film.setDuration(-50);
+        film.setDescription("Film description");
+        film.setReleaseDate(LocalDate.of(2000, 8, 20));
+        FilmController filmController = new FilmController();
+        assertThrows(ValidationException.class, () -> filmController.createFilm(film));
+    }
 
 }
