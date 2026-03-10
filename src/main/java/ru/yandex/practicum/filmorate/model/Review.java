91882a0a6d24e8ebede80695bfa62d7075d18db9
VeraBes <bespalaya.vera@gmail.com>
2026-03-10 16:44:19 +0300

    закомментированы строки в файле data.sql, добавлена аннотация @NotNull

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,13 +15,11 @@ public class Review {
     private int reviewId;
     @NotBlank
     private String content;
-
     @JsonProperty("isPositive")
     private boolean isPositive;
-
+    @NotNull
     private int userId;
-
+    @NotNull
     private int filmId;
-
     private int useful;
 }
